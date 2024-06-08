19be1255a95cbe73ab858b9bed115e2e8d8129bd
Sriramkumar S <118042014+Sriramkumar-S@users.noreply.github.com>
2024-06-08 18:29:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,114 @@
-var div = document.createElement('div');
-div.style.backgroundColor = 'aqua';
-div.style.display = 'inline'
-div.innerHTML = "This is a div element";
-document.body.append(div)
+// var div = document.createElement('div');
+// div.style.backgroundColor = 'aqua';
+// div.style.display = 'inline'
+// div.innerHTML = "This is a div element";
+// document.body.append(div)
 
-var lineBreak = document.createElement('br');
-document.body.append(lineBreak)
+// var lineBreak = document.createElement('br');
+// document.body.append(lineBreak)
 
-div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'yellow';
-})
+// div.addEventListener('mouseover', () => {
+//     div.style.backgroundColor = 'yellow';
+// })
 
-div.addEventListener('mouseout', () => {
-    div.style.backgroundColor = 'aqua';
-})
+// div.addEventListener('mouseout', () => {
+//     div.style.backgroundColor = 'aqua';
+// })
 
-var button = document.createElement('button');
-button.innerHTML = "Click Me!";
-button.style.display = 'block';
-button.addEventListener('click', () => {
-    foo();
-})
-document.body.append(button)
-document.body.append(lineBreak)
+// var button = document.createElement('button');
+// button.innerHTML = "Click Me!";
+// button.style.display = 'block';
+// button.addEventListener('click', () => {
+//     foo();
+// })
+// document.body.append(button)
+// document.body.append(lineBreak)
 
-function foo() {
-    var input = prompt("Enter your name");
+// function foo() {
+//     var input = prompt("Enter your name");
 
-    var span = document.createElement('span');
-    span.innerHTML = input;
-    document.body.append(span)
+//     var span = document.createElement('span');
+//     span.innerHTML = input;
+//     document.body.append(span)
+// }
+
+
+// var textArea = document.createElement('textarea');
+// document.body.append(textArea);
+// textArea.addEventListener('input', () => {
+//     console.log(textArea.value.length)
+// })
+
+
+
+////////////////////////////////////////////////////////////
+
+// Label
+function labelCreate(forAttrValue, labelValue) {
+    let label = document.createElement('label');
+    label.setAttribute('for', forAttrValue);
+    label.innerHTML = labelValue;
+    return label;
+}
+// Inputs
+// FirstName // MiddleName // LastName // Password // FileUpload
+
+function inputCreate(tagName, typeName, inputId) {
+    let input = document.createElement(tagName);
+    input.setAttribute('type', typeName);
+    input.id = inputId;
+    return input;
 }
 
+function lineBreak() {
+    let line_break = document.createElement('br');
+    return line_break;
+}
+let line_Break = lineBreak();
 
-var textArea = document.createElement('textarea');
-document.body.append(textArea);
-textArea.addEventListener('input', () => {
-    console.log(textArea.value.length)
-})
+let firstName_line_Break = lineBreak();
+let firstNameInput_line_Break = lineBreak();
+let firstNameLabel = labelCreate("firstName", "First Name: ")
+let firstNameInput = inputCreate("input", "text", "firstName" )
+
+let middleName_line_Break = lineBreak();
+let middleNameInput_line_Break = lineBreak();
+let middleNameLabel = labelCreate("middleName", "Middle Name: ")
+let middleNameInput = inputCreate("input", "text", "middleName" )
+
+let lastName_line_Break = lineBreak();
+let lastNameInput_line_Break = lineBreak();
+let lastNameLabel = labelCreate("lastName", "Last Name: ")
+let lastNameInput = inputCreate("input", "text", "lastName" )
+
+let password_line_Break = lineBreak();
+let passwordInput_line_Break = lineBreak();
+let passwordLabel = labelCreate("password", "Password: ")
+let passwordInput = inputCreate("input", "password", "password" )
+
+let fileUpload_line_Break = lineBreak();
+let fileUploadLabel = labelCreate("fileUpload", "Browse Files: ")
+let fileUploadInput = inputCreate("input", "file", "fileUpload" )
+
+document.body.append(firstNameLabel, firstName_line_Break, firstNameInput);
+document.body.append(firstNameInput_line_Break);
+document.body.append(middleNameLabel, middleName_line_Break, middleNameInput);
+document.body.append(middleNameInput_line_Break);
+document.body.append(lastNameLabel, lastName_line_Break, lastNameInput);
+document.body.append(lastNameInput_line_Break);
+document.body.append(passwordLabel, password_line_Break, passwordInput);
+document.body.append(passwordInput_line_Break);
+document.body.append(fileUploadLabel, fileUpload_line_Break, fileUploadInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
